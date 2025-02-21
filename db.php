3d3fe6a2e6a9e463a3308738853ced16c6847115
Drew-bautista <142853218+Drew-bautista@users.noreply.github.com>
2025-02-22 00:56:39 +0800

    Create db.php

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+$host = "ecommerce-production-60ec.up.railway.app";  // Palitan kung iba ang host sa Railway
+$port = 3306;                      // Default MySQL port
+$dbname = "ecommerceweb";     // Palitan ng tamang database name
+$username = "root";        // Palitan ng username mula Railway
+$password = "dQZkrYPhOtqUDcChjzMjKkperiBTSuWU";        // Palitan ng password mula Railway
+
+try {
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo " Database Connected Successfully!";
+} catch(PDOException $e) {
+    echo " Connection failed: " . $e->getMessage();
+}
+?>
